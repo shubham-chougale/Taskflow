606b881f7b45b63bf4b1bf2a3680c483ec3ab0e5
Shubham Chougale <shubham.chougale@coditas.com>
2026-01-12 13:11:23 +0530

    project structure

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from app.api import auth, tasks
+from app.api import auth_routes, task_routes
 
 app = FastAPI(title="TaskFlow")
 
-app.include_router(auth.router)
-app.include_router(tasks.router)
+app.include_router(auth_routes.router)
+app.include_router(task_routes.router)
